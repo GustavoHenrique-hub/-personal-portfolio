02f17c1aff19b2823ee88c43a62b50c3dce6a4b4
Arthur <arthurferreira1417@gmail.com>
2024-03-04 21:17:19 -0300

    feat: adding a canvas configuration

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+var canvas = document.querySelector("canvas");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var ctx = canvas.getContext("2d");
+
+ctx.fillRect(100, 100, 100, 100);
+ctx.fillRect(400, 100, 100, 100);
+ctx.fillRect(300, 200, 100, 100);
